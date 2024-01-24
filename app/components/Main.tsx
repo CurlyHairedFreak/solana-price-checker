4263a8f5c28645f4b288d3692adcb0d0b42b5d00
CurlyHairedFreak <152513491+CurlyHairedFreak@users.noreply.github.com>
2024-01-24 10:53:27 +0000

    Update Main.tsx

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -8,12 +8,12 @@ const Main = () => {
   const { solState, icon } = useFetchPrice();
 
   return (
-    <main className="mt-12 text-lg">
+    <main className="mt-12 text-lg text-cyan-300">
       {solState.previousSolPrice === 0.0 && <div className="loader"></div>}
       {solState.currentSolPrice !== 0.0 && (
         <div className="flex items-center gap-2 justify-center">
           <h3 className="text-cyan-300 ">Solana: </h3>
-          <p>$ {solState.currentSolPrice}</p>
+          <p> $ {solState.currentSolPrice}</p>
           <FontAwesomeIcon
             icon={icon}
             className={
